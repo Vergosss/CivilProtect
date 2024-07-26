d6114cd1d9394ca4714b38bd0e8459f19b6086e1
Georgios Vergos <72676680+Vergosss@users.noreply.github.com>
2024-07-26 03:39:54 +0300

    Read file and insert items and categories into DB.

--- server.js
+++ server.js
@@ -177,10 +177,49 @@ app.post('/signup',(req,res)=>{
 const fs = require('fs');
 //
 app.get('/test/', (req,res)=>{
-let data =  fs.readFile('/uploads/upload-1721913498358.json','utf-8');
-data = JSON.parse(data);
-const {code,message,categories,...items} = data;
-console.log(items);
+fs.readFile(__dirname + '/uploads/upload-1721913498358.json','utf-8',(error,data)=>{
+//asygxrona diavase to arxeio(exei callback)
+	if(error) throw error;
+	data = JSON.parse(data);//diavase ta dedomena os json
+	let {code,message,categories,...items} = data;//afairo apo to arxeio to code,message,categories gia na xeiristo mono ta proionta
+	//kai menoun mono ta items
+	//let {details,...rest} = items;
+	//console.log(items);
+	//console.log(data);
+	//console.log(rest);
+	const final_items = items.items.map(({details,...rest})=>rest);//epestrepse to rest
+	//afairo to details kathe eggrafhs-json object-stixiou tou array meso ths map
+	//to items  epistrefei {items:[...]} to items.items episrefei [...]
+	//console.log(final_items);
+	// /*
+	for(let item of final_items){
+		connection.query('INSERT INTO Item VALUES(?,?,?)',[item.id,item.name,item.category],(error,results)=>{
+			if(error) throw error;
+			if(results.affectedRows>0){
+				console.log('Success!');
+			}//diatrexo to array twn objects 
+			else{
+				console.log('Failed');
+			}
+		});
+		
+	}
+
+// */
+console.log(categories);
+	for(let category of categories){
+		connection.query('INSERT INTO Category VALUES(?,?)',[category.id,category['category_name']],(error,results)=>{
+			if(error) throw error;
+			if(results.affectedRows>0){
+				console.log('Success!');
+			}
+			else{
+				console.log('Error!');
+			}
+		});
+	}
+});
+
 });
 //
 app.get('/coordinates/', (req,res)=>{
