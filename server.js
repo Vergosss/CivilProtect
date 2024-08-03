0c2455f124764da8bbc20b6cb8ede62930949eaf
Georgios Vergos <72676680+Vergosss@users.noreply.github.com>
2024-08-04 02:17:22 +0300

    More of admin...

--- server.js
+++ server.js
@@ -436,7 +436,7 @@ app.get('/get_requests/',(req,res)=>{
 //
 app.get('/fetch_requests/',(req,res)=>{
 
-	connection.query('SELECT citizen_first_name,citizen_last_name,citizen_telephone,entry_date,item,quantity,withdrawal_date,vehicle_username,ST_X(cords),ST_Y(cords) FROM Request',(error,results)=>{
+	connection.query('SELECT citizen_first_name,citizen_last_name,citizen_telephone,entry_date,item,quantity,withdrawal_date,vehicle_username,ST_X(cords),ST_Y(cords),lifted FROM Request',(error,results)=>{
 		if(error) throw error;
 		res.send(results);
 	});
